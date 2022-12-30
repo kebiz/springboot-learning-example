c90ecf208aecb2b9337ceecf788172a917591ac3
zengzp <123@123.com>
2022-12-30 17:31:41 +0800

    构建购物车

--- learning-cart-service/src/main/java/com/zengzp/cart/entity/OrderItem.java
+++ learning-cart-service/src/main/java/com/zengzp/cart/entity/OrderItem.java
@@ -1,5 +1,11 @@
 package com.zengzp.cart.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author ：zengzhipeng
  * @date ：Created in 2022/12/30 15:17
@@ -7,5 +13,40 @@ package com.zengzp.cart.entity;
  * @modified By：
  * @version: 1$
  */
-public class OrderItem {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem implements Serializable {
+    //id
+    private long id;
+    //skuId
+    private long skuId;
+    //spuId
+    private long spuId;
+    //订单ID
+    private String orderId;
+    //一级分类Id
+    private long category1Id;
+    //二级分类Id
+    private long category2Id;
+    //三级分类Id
+    private long category3Id;
+    //名称
+    private String name;
+    //数量
+    private Integer num;
+    //价格
+    private Double price;
+    //重量
+    private Double weight;
+    //总价
+    private Double totalMoney;
+    //实付金额
+    private Double payMoney;
+    //图片地址
+    private String image;
+    //运费
+    private Double postFee;
+    //是否退货
+    private String isReturn;
 }
--- learning-cart-service/src/main/java/com/zengzp/cart/entity/Spu.java
+++ learning-cart-service/src/main/java/com/zengzp/cart/entity/Spu.java
@@ -1,5 +1,11 @@
 package com.zengzp.cart.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author ：zengzhipeng
  * @date ：Created in 2022/12/30 15:17
@@ -7,5 +13,55 @@ package com.zengzp.cart.entity;
  * @modified By：
  * @version: 1$
  */
-public class Spu {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Spu implements Serializable {
+    //ID
+    private long id;
+    //名称
+    private String name;
+    //副标题
+    private String caption;
+    //货号
+    private String sn;
+    //品牌Id
+    private long brandId;
+    //品牌名称
+    private String brandName;
+    //一级分类Id
+    private long category1Id;
+    //二级分类Id
+    private long category2Id;
+    //三级分类Id
+    private long category3Id;
+    //模板Id
+    private long templateId;
+    //运费模板Id
+    private long freightId;
+    //图片
+    private String image;
+    //图片列表
+    private String images;
+    //售后服务
+    private String saleService;
+    //介绍
+    private  String introduction;
+    //规格列表
+    private String spacItems;
+    //参数列表
+    private String paraItems;
+    //销量
+    private Integer saleNum;
+    //评论数
+    private Integer commentNum;
+    //是否上架
+    private String isMarketable;
+    //是否启用规格
+    private  String isEnableSpac;
+    //是否删除
+    private  String isDelete;
+    //审核状态
+    private String status;
+
 }
