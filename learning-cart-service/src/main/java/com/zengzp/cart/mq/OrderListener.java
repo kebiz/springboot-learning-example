9b1baff11cb38dfd2009ebda04a4f037117f6761
zengzp <123@123.com>
2023-04-21 21:34:39 +0800

    rabbitmq分布式事务

--- learning-cart-service/src/main/java/com/zengzp/cart/mq/OrderListener.java
+++ learning-cart-service/src/main/java/com/zengzp/cart/mq/OrderListener.java
@@ -2,13 +2,17 @@ package com.zengzp.cart.mq;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Assert;
+import com.learning.code.common.consumer.BaseConsumer;
 import com.learning.code.common.contant.OrderQueueNameConstant;
 import com.learning.code.common.model.CreateOrderMessage;
 import com.learning.code.common.model.MessageSendLog;
 import com.learning.code.common.model.OrderFailMessage;
 import com.learning.code.common.model.SynStockDBMessage;
+import com.learning.code.common.proxy.BaseConsumerProxy;
+import com.learning.code.common.util.MessageHelper;
 import com.learning.dubbo.MessageSendLogService;
 import com.rabbitmq.client.Channel;
+import com.zengzp.cart.mq.consumer.SynStockDBConsumer;
 import com.zengzp.cart.service.OrderTestService;
 import com.zengzp.cart.service.SkuService;
 import lombok.extern.slf4j.Slf4j;
@@ -38,34 +42,20 @@ import java.util.*;
 @Component
 public class OrderListener {
     @Autowired
-    private SkuService skuService;
+    private SynStockDBConsumer synStockDBConsumer;
     @Resource
     private MessageSendLogService messageSendLogService;
     @RabbitListener(queues = OrderQueueNameConstant.SYN_STOCK_DB)
-    public void createOrder(SynStockDBMessage synStockDBMessage, CorrelationData correlationData, Channel channel, Message message) throws IOException {
+    public void synStockDB(Channel channel, Message message) throws IOException {
         log.info("=========生产==========收到同步库存数据消息:deliveryTag{},当前时间{},消息内容{}.", message.getMessageProperties().getDeliveryTag(),
                 DateUtil.now(),
-                synStockDBMessage.toString());
-        try {
-            Assert.notNull(synStockDBMessage.getOrderId(),"订单ID不能为空");
-            Assert.notEmpty(synStockDBMessage.getStockDtoList(),"扣减库存数据不能为空");
-            //判断幂等性
-            MessageSendLog msgSendLog = messageSendLogService.getMsgSendLogByMsgId(correlationData.getId());
-            if(msgSendLog==null || "3".equals(msgSendLog.getSendStatus())){
-                log.info("重复消费-msgId：{}",correlationData.getId());
-                return;
+                MessageHelper.msgToObj(message,SynStockDBMessage.class));
+            BaseConsumerProxy baseConsumerProxy = new BaseConsumerProxy(synStockDBConsumer, messageSendLogService);
+            BaseConsumer proxy = (BaseConsumer) baseConsumerProxy.getProxy();
+            if (null != proxy) {
+                proxy.consume(message, channel);
             }
-            skuService.synStockDB(synStockDBMessage.getStockDtoList());
-            log.info("同步库存执行成功: deliveryTag{}", message.getMessageProperties().getDeliveryTag());
-            messageSendLogService.updateMsgStatus(correlationData.getId(),"3");
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
 
-        }catch (Exception ex){
-            log.error(ex.getMessage(), ex);
-            log.info("同步库存执行失败: deliveryTag{}", message.getMessageProperties().getDeliveryTag());
-
-            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
-        }
     }
 
 }
--- learning-code-controller/src/main/java/com/zengzp/product/mq/OrderListener.java
+++ learning-code-controller/src/main/java/com/zengzp/product/mq/OrderListener.java
@@ -12,6 +12,7 @@ import com.learning.code.common.util.MessageHelper;
 import com.learning.dubbo.MessageSendLogService;
 import com.rabbitmq.client.Channel;
 import com.zengzp.product.mq.consumer.OrderConsumer;
+import com.zengzp.product.mq.consumer.ReturnStockConsumer;
 import com.zengzp.product.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.asm.Advice;
@@ -42,6 +43,8 @@ public class OrderListener {
     private MessageSendLogService messageSendLogService;
     @Autowired
     private OrderConsumer orderConsumer;
+    @Autowired
+    private ReturnStockConsumer returnStockConsumer;
     @RabbitListener(queues = OrderQueueNameConstant.ORDER_CREATE)
     public void createOrder(Channel channel, Message message) throws IOException {
         log.info("==========生产================收到订单创建消息DeliveryTag:{},当前时间{},消息内容{}.", message.getMessageProperties().getDeliveryTag(),
@@ -55,24 +58,14 @@ public class OrderListener {
     }
 
     @RabbitListener(queues = OrderQueueNameConstant.ERROR_QUEUE)
-    public void dealErrorMessage(CreateOrderMessage createOrderMessage, Channel channel, Message message) throws IOException {
-        log.info("=========生产==========收到订单创建失败消息:CorrelationId{},当前时间{},消息内容{}.", message.getMessageProperties().getCorrelationId(),
+    public void dealErrorMessage(Channel channel, Message message) throws IOException {
+        log.info("=========生产==========收到回退库存消息:CorrelationId{},当前时间{},消息内容{}.", message.getMessageProperties().getCorrelationId(),
                 DateUtil.now(),
-                createOrderMessage.toString());
-        try {
-
-           // messageSendLogService.saveMsgSendLog(keyUpper, JSONUtil.toJsonPrettyStr(createOrderMessage),"-2");
-            /* if(orderService.returnStock(createOrderMessage.getOrderId())){
-               log.info("订单回退执行成功: deliveryTag{}", message.getMessageProperties().getDeliveryTag());
-               channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
-           }else {
-               throw new RuntimeException("订单回退报错");
-           }*/
-        }catch (Exception ex){
-            log.error(ex.getMessage(), ex);
-            log.info("处理异常队列消息失败: deliveryTag{},发送邮件到管理员", message.getMessageProperties().getDeliveryTag());
-
-            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                MessageHelper.msgToObj(message,CreateOrderMessage.class));
+        BaseConsumerProxy baseConsumerProxy = new BaseConsumerProxy(returnStockConsumer, messageSendLogService);
+        BaseConsumer proxy = (BaseConsumer) baseConsumerProxy.getProxy();
+        if (null != proxy) {
+            proxy.consume(message, channel);
         }
 
     }
